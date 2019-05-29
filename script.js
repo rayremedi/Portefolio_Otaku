f68caa36640476c29f7f753bcc333f8305bb8eb6
rayremedi <ray.remedi17@laposte.net>
2019-05-29 12:04:20 +0200

    first push

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleForm() {
+    var formulaire = document.querySelector('.formulaire');
+    if (formulaire.style.display == 'block') {
+        formulaire.style.display = 'none';
+
+    } else {
+        formulaire.style.display = 'block';
+    }
+}
